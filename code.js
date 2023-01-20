8112c89f3ab82a0d06a6b09bd418b645b1d9953b
Kosty20 <104622649+Kosty20@users.noreply.github.com>
2023-01-20 16:41:06 +0200

    Finished

--- code.js
+++ code.js
@@ -1,22 +1,30 @@
 
-const currentTime = new Date;
-
 const hrBox = document.querySelector('.hours');
 const minBox = document.querySelector('.minutes');
 const secBox = document.querySelector('.seconds');
+const antePost = document.querySelector('.ante-post');
 
-let hours = currentTime.getHours() - 12;
-let minutes = currentTime.getMinutes();
-let seconds = currentTime.getSeconds();
+let currentTime;
+let hours;
+let minutes;
+let seconds;
 
+getClock();
 
-if(hours < 10){
-    hrBox.innerText = `0${hours}`
-} else {
-    hrBox.innerText = `${hours}`
-}
-if(minutes < 10){
-    minBox.innerText = `0${minutes}`
-} else {
-    minBox.innerText = `${minutes}`
+setInterval(getClock, 1000)
+
+function getClock() {
+    currentTime = new Date;
+    hours = currentTime.getHours();
+    minutes = currentTime.getMinutes();
+    seconds = currentTime.getSeconds();
+
+    if(hours > 12){
+        hours -= 12;
+        antePost.innerText = 'PM';
+    }
+
+    hours < 10 ? hrBox.innerText = `0${hours}` : hrBox.innerText = `${hours}`;
+    minutes < 10 ? minBox.innerText = `0${minutes}` : minBox.innerText = `${minutes}`;
+    seconds < 10 ? secBox.innerText = `0${seconds}` : secBox.innerText = `${seconds}`;
 }
